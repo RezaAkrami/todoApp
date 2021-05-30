8b0559168a0e6bf80c423ceef32ee38843877ee9
Reza <rezaakrami32@gmail.com>
2021-05-30 20:39:04 +0430

    Define variable in js file

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// Define variable
+
+let form = document.querySelector('#task-fomr');
+let taskInput = document.querySelector('#task-form__input');
+let tasksList = document.querySelector('#task-list');
+let clearAllTasks = document.querySelector('clear-all-tasks');
