2c4dbb6c84e7c4cc25d5d9bb8151ecac12d7d55c
Reza <rezaakrami32@gmail.com>
2021-05-30 20:55:45 +0430

    Add Event to form when its submitted

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,27 @@
 // Define variable
 
-let form = document.querySelector('#task-fomr');
+let form = document.querySelector('#task-form');
 let taskInput = document.querySelector('#task-form__input');
 let tasksList = document.querySelector('#task-list');
 let clearAllTasks = document.querySelector('clear-all-tasks');
+
+// Add Event Listener when form submited
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    let taskItem;
+
+    taskItem = `
+    <li class="task-list__item">
+
+                <h2>${taskInput.value}</h2>
+
+                <div class="icons">
+                    <a href="#" class="check__icon"><i class="far fa-check-square"></i></a>
+                    <a href="#" class="delete__icon"><i class="far fa-trash-alt"></i></a>
+                </div>
+            </li>
+    `
+    tasksList.innerHTML+=taskItem;
+})
