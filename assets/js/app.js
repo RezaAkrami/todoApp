53b0afe232d27ec058e34a5fa929fcb8eadacc76
Reza <rezaakrami32@gmail.com>
2021-05-30 22:31:47 +0430

    add Event for delet or check task

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,32 +6,32 @@ let tasksList = document.querySelector('#task-list');
 let clearAllTasks = document.querySelector('clear-all-tasks');
 
 // Add Event Listener when form submited
-
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
 
-    let taskItem;
-
-    taskItem = `
+// Add task to the list item
+    let taskItem = `
     <li class="task-list__item">
 
                 <h2>${taskInput.value}</h2>
 
                 <div class="icons">
-                    <i class="far fa-check-square check__icon"></i>
-                    <i class="far fa-trash-alt delete__icon"></i>
+                <a href="#" class=""><i class="far fa-check-square check__icon"></i></a>
+                <a href="#" class="delete__icon"><i class="far fa-trash-alt "></i></a>
                 </div>
             </li>
     `
-    tasksList.innerHTML+=taskItem;
+    tasksList.innerHTML+=taskItem;  
+})
 
-    // Define the variable after submitting the form
-    let checkIcon = document.querySelector('.check__icon');
-    let deleteIcon = document.querySelector('.delete__icon');
+// Add Event Listener when The user wants to delete or Check the task
+tasksList.addEventListener('click',(e)=>{
 
+    e.target.classList.toggle('checked');
 
-    // Add Event Listener when The user wants to check the task
-    checkIcon.addEventListener('click',(e)=>{           
-        e.target.classList.toggle('checked');
-    })
+    if(e.target.parentElement.classList.contains('delete__icon')){
+
+        e.target.parentElement.parentElement.parentElement.remove();
+
+    }
 })
